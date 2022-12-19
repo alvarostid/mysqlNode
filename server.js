8b159de4266ca40e9b9e3f2b6c9a07c062162bc1
unknown <a8stid@gmail.com>
2022-12-19 09:16:39 -0500

    firts commit

---
+++ server.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+
+const express=require('express')
+const mysql=require('mysql2')
+const myconn=require('express-myconnection')
+const routes=require('./routes')
+var cors=require('cors')
+
+
+
+const app=express()
+app.use(cors())
+
+app.set('port',process.env.PORT_SERVER || 9000)
+
+const dbOptions={
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || '3306',
+    user: process.env.BD_USER || 'root',
+    password: process.env.DB_PWD || '',
+    database: process.env.DB_NAME || 'library'
+}
+/// middelwares-------
+
+app.use(myconn(mysql,dbOptions,'single'))
+app.use(express.json())/// formato de entrega
+
+
+//// routes ---------
+app.get('/',(req,res)=>{
+    res.send('Welcome to my APP 2022')
+ })
+
+ app.use('/api',routes)
+ 
+
+ app.listen(app.get('port'),()=>{
+    console.log(`El puerto corre en: ${app.get('port')}`)
+})
